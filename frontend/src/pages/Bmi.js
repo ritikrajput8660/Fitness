a622d851f5a3db5ecff5a30a930371743dff19aa
ritik <rajputritik8660@gmail.com>
2025-04-08 14:42:59 +0530

    bmi_css

--- frontend/src/pages/Bmi.js
+++ frontend/src/pages/Bmi.js
@@ -4,81 +4,79 @@ import '../styles/bmicss.css'
 import React, { useState } from 'react';
 
 function BMICalculator() {
-  const [height, setHeight] = useState("");
-  const[weight, setWeight] = useState("");
-  const [bmi, setBmi] = useState(null);
-  const [category, setCategory] = useState("");
-  const [categoryColor,setCategoryColor] = useState("");
+    const [height, setHeight] = useState("");
+    const [weight, setWeight] = useState("");
+    const [bmi, setBmi] = useState(null);
+    const [category, setCategory] = useState("");
+    const [categoryColor, setCategoryColor] = useState("");
 
-  const calculateBMI = ()=>{
-      if(height && weight){
-          const heightInMeters = height / 100;
-          const bmi = (weight / (heightInMeters * heightInMeters)).toFixed(2);
-          setBmi(bmi);
+    const calculateBMI = () => {
+        if (height && weight) {
+            const heightInMeters = height / 100;
+            const bmi = (weight / (heightInMeters * heightInMeters)).toFixed(2);
+            setBmi(bmi);
 
-          if(bmi < 18.5){
-              setCategory("You are Underweight Increase you diet");
-              setCategoryColor("rgb(219, 210, 25)");
-          }
-          else if(bmi >= 18.5 && bmi <= 24.9){
-              setCategory("Normal weight ");
-              setCategoryColor(" #22c55e");
-          }else if(bmi >= 25 && bmi <= 29.9){
-              setCategory("Overweight");
-              setCategoryColor("rgb(239, 144, 144)");
-          }else{
-              setCategory("You are Obese go to the Gym");
-              setCategoryColor(" #ef4444");
-          }
-      }
-  }
+            if (bmi < 18.5) {
+                setCategory("You are Underweight Increase you diet");
+                setCategoryColor("rgb(219, 210, 25)");
+            }
+            else if (bmi >= 18.5 && bmi <= 24.9) {
+                setCategory("Normal weight ");
+                setCategoryColor(" #22c55e");
+            } else if (bmi >= 25 && bmi <= 29.9) {
+                setCategory("Overweight");
+                setCategoryColor("rgb(239, 144, 144)");
+            } else {
+                setCategory("You are Obese go to the Gym");
+                setCategoryColor(" #ef4444");
+            }
+        }
+    }
 
-return (
-  
-  <div className='bmi-container'>
+    return (
 
-
-
-
-<div className='bmi-card'>
-      <h2>BMI Calculator</h2>
-      <div className='input-group'>
-          <label>Weight (kg)</label>
-          <input 
-           type='number'
-           value={weight}
-           onChange={(e) => setWeight(e.target.value)}
-           placeholder='Enter your weight'
-          />
-      </div>
-      <div className='input-group'>
-          <label>Height (cm)</label>
-          <input
-              type='number'
-              value={height}
-              onChange={(e) => setHeight(e.target.value)}
-              placeholder='Enter your height'
-          />
-      </div>
-      <button className='btn-calculator' onClick={calculateBMI}>Calculate BMI</button>
-      {
-          bmi && (
-              <div className='result'>
-                  <h2>Your BMI: {bmi}</h2>
-                  <h2 style={{color:categoryColor}}>Category: {category}</h2>
-              </div>
-          )
-      }
-  </div>
+        <div className='bmi-container'>
+            <div className='bmi-card'>
+                <h2>BMI Calculator</h2>
+                <div className='input-group'>
+                    <label>Weight (kg)</label>
+                    <input
+                        type='number'
+                        value={weight}
+                        onChange={(e) => setWeight(e.target.value)}
+                        placeholder='Enter your weight'
+                        min="0"
+                    />
+                </div>
+                <div className='input-group'>
+                    <label>Height (cm)</label>
+                    <input
+                        type='number'
+                        value={height}
+                        onChange={(e) => setHeight(e.target.value)}
+                        placeholder='Enter your height'
+                        min="0"
+                    />
+                </div>
+                <button className='btn-calculator' onClick={calculateBMI}>Calculate BMI</button>
+                {
+                    bmi && (
+                        <div className='result'>
+                            <h2>Your BMI: {bmi}</h2>
+                            <h2 style={{ color: categoryColor }}>Category: {category}</h2>
+                        </div>
+                    )
+                }
+            </div>
 
 
 
 
-  </div>
+        </div>
 
-  
-  
-)
+
+
+    )
 }
 
 export default BMICalculator
